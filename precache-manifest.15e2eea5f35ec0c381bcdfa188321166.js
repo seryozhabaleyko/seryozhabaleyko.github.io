7a3b97c4b75dd0f90b021bec3b3d8833a771ee51
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-06-22 16:37:32 +0300

    build

---
+++ precache-manifest.15e2eea5f35ec0c381bcdfa188321166.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "549ec463d51c37511c8bab3af37104ff",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/index.html"
+  },
+  {
+    "revision": "43fe8214d9af767eb15d",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/static/css/2.1fcf08fb.chunk.css"
+  },
+  {
+    "revision": "97c1842388ad23336ebf",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/static/css/main.ae54585f.chunk.css"
+  },
+  {
+    "revision": "43fe8214d9af767eb15d",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/static/js/2.d7c0df05.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/static/js/2.d7c0df05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97c1842388ad23336ebf",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/static/js/main.c99d4646.chunk.js"
+  },
+  {
+    "revision": "2d680ec1aa66e09485f4",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/static/js/runtime-main.cac0c25d.js"
+  },
+  {
+    "revision": "5dbed5ff57c87d9e38b00e0df9f11c25",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/static/media/Iam.5dbed5ff.jpg"
+  },
+  {
+    "revision": "8d7ae5ac6e934401e57e82f70b996bc1",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/static/media/ab-img.8d7ae5ac.png"
+  },
+  {
+    "revision": "625aa9cbe9816186b01cb392b3f418bd",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/static/media/belhard.625aa9cb.jpg"
+  },
+  {
+    "revision": "f43f95d1301e65a519228a99b47368e0",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/static/media/clock.f43f95d1.jpg"
+  },
+  {
+    "revision": "ed5e3719d2f05e0703ac89680509788b",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/static/media/country.ed5e3719.jpg"
+  },
+  {
+    "revision": "617870e422d4b75087cdcf9ff298032e",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/static/media/github.617870e4.svg"
+  },
+  {
+    "revision": "881de89d2e76aab947f89290935288e0",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/static/media/githubSearch1.881de89d.jpg"
+  },
+  {
+    "revision": "9916240f4272f7eeb4ae152d87f488f9",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/static/media/kanban.9916240f.jpg"
+  },
+  {
+    "revision": "619231ff8ec7a6b4d39aede30fa9890e",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/static/media/linkedin.619231ff.svg"
+  },
+  {
+    "revision": "39d1a4e6e6715ce3f54ef6800ebade20",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/static/media/mail.39d1a4e6.svg"
+  },
+  {
+    "revision": "1d1f5ddccde07e82555324597c039bb8",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/static/media/medium.1d1f5ddc.jpg"
+  },
+  {
+    "revision": "00a7a0c9dd6d4426e45067e2e0a109e6",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/static/media/phone.00a7a0c9.svg"
+  },
+  {
+    "revision": "257d09686af38ac69ce2ee966091c112",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/static/media/pianino.257d0968.jpg"
+  },
+  {
+    "revision": "95b294c2b641444021da9b9325e58606",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/static/media/portfolio.95b294c2.jpg"
+  },
+  {
+    "revision": "383451fe2303020717667c22f12fe4cb",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/static/media/repairs.383451fe.jpg"
+  },
+  {
+    "revision": "369e60e8f76e570dbc734bbf9f41dc98",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/static/media/tennis.369e60e8.jpg"
+  },
+  {
+    "revision": "be06689853aec102607f5e54e6fd2dfa",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/static/media/twitter.be066898.svg"
+  },
+  {
+    "revision": "2790ad20a8c9d1a167a9322c5fdbe12a",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/static/media/typing-speed.2790ad20.png"
+  },
+  {
+    "revision": "171c8efea95ecc97796a5d63ac98af8d",
+    "url": "/seryozhabaleyko/seryozhabaleyko.github.io/static/media/vk.171c8efe.svg"
+  }
+]);
--- src/components/Page/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './Page.scss';
-
-function Page({ children }) {
-    return <div className="page">{children}</div>;
-};
-
-export default Page;
